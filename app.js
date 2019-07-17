04969f5ead04569860b1d5052d4b564b02c62ba0
Flávio Batista <flaviowander@msn.com>
2019-07-17 07:24:05 -0300

    Correção de image serving

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var session = require("express-session");
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser = require("body-parser");
-var cors = require('cors')
+var cors = require('cors');
 
 var apiRouter = require('./routes/api/api');
 
@@ -25,11 +25,8 @@ app.use(bodyParser.json());
 
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-
 app.use(cors());
-
+app.use('/files', express.static('files'));
 app.use('/api', apiRouter);
 
 app.use(function (req, res, next) {
